c0bad826d6791959cc56f40571a5198256e065fb
Vlad <vladborsh95s322@gmail.com>
2016-02-09 16:06:35 +0200

    drag the oval

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,16 @@
 package sample;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -12,23 +18,48 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    double xPos = 100;
+    double yPos = 100;
+
+    EventHandler<MouseEvent> mouseHandler = new EventHandler<MouseEvent>() {
+
+        @Override
+        public void handle(MouseEvent mouseEvent) {
+            xPos = mouseEvent.getSceneX();
+            yPos = mouseEvent.getSceneY();
+        }
+
+    };
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        StackPane root = new StackPane();
+        primaryStage.setTitle( "AnimatedImage Example" );
 
-        Rectangle rect = new Rectangle(0, 0, 100, 100);
-        rect.setFill(Color.GREENYELLOW);
+        Group root = new Group();
+        Scene theScene = new Scene( root );
+        primaryStage.setScene( theScene );
 
-        DropShadow ds = new DropShadow(15, Color.DARKGREEN);
+        theScene.setOnMouseMoved(mouseHandler);
+        Canvas canvas = new Canvas( 512, 512 );
+        root.getChildren().add( canvas );
 
-        rect.setEffect(ds);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
 
-        root.getChildren().add(rect);
+        final long startNanoTime = System.nanoTime();
 
-        Scene scene = new Scene(root, 250, 200, Color.WHITESMOKE);
+        new AnimationTimer()
+        {
+            public void handle(long currentNanoTime)
+            {
+                gc.clearRect(0, 0, 512, 512);
+                double t = (currentNanoTime - startNanoTime) / 1000000000.0;
 
-        primaryStage.setTitle("DropShadow");
-        primaryStage.setScene(scene);
+                gc.setFill(Color.FORESTGREEN);
+                gc.fillOval(xPos-40, yPos-40, 80, 80);
+                gc.restore();
+            }
+        }.start();
+
         primaryStage.show();
     }
 
